adb612f6079e2f70f87ce3452526b87b368a30da
Aaron Feng <aaron.feng@gmail.com>
2009-09-05 22:53:45 -0400

    print rabbit status on trixx-core-repl.sh startup

--- src/main/clj/com/leftrightfold/trixx/main.clj
+++ src/main/clj/com/leftrightfold/trixx/main.clj
@@ -1,6 +1,9 @@
 (ns com.leftrightfold.trixx.main
+  (:require [com.leftrightfold.trixx.core :as trixx])
   (:gen-class))
 
 (defn -main [& args]
   (prn "Welcome to Trixx Repl")
+  (prn "Rabbit status:")
+  (prn (trixx/status))
   (clojure.lang.Repl/main (into-array String args)))
