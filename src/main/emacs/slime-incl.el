c17a3ce8e9d3f8ac4f96e427a668909cddca6523
Aaron Feng <aaron.feng@gmail.com>
2009-10-26 20:34:08 -0400

    removed slime and upgraded deps to 0.2

--- src/main/emacs/slime-incl.el
+++
@@ -1,6 +0,0 @@
-(add-to-list 'slime-lisp-implementations
-              '(trixx-core ("@bin.dir@/trixx-core-repl")
-                         :init swank-clojure-init
-                         :init-function krb-swank-clojure-init)
-
-               t)
